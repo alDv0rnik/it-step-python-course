bc83fefbbc6ecff304ec4d735dd4a4f252dad536
Artem <mvamvh@gmail.com>
2022-11-26 01:16:21 +0300

    Homework_11

---
+++ task_3.py
@@ -0,0 +1,9 @@
+
+name_s = lambda name = input().capitalize(), surname = input().capitalize(): print(f"Меня зовут {name} {surname}.")
+
+name_s()
+
+
+# name_s = lambda name, surname: print(f"Меня зовут {name} {surname}.")
+
+# name_s(input().capitalize(), input().capitalize())
